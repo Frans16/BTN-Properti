0bcefd6afd6cfce60adcc1e370da1a8bf2e17368
Frans16 <franstmarbun@gmail.com>
2024-01-23 14:48:15 +0700

    update class naming

--- src/main/java/org/btn/locators/CalculatePriceLoct.java
+++ src/main/java/org/btn/locators/CalculatePriceLoct.java
@@ -3,22 +3,22 @@ package org.btn.locators;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HitungHargaLocator {
+public class CalculatePriceLoct {
 	
 	@FindBy(xpath = "//input[@placeholder='Penghasilan Total']")
-	public WebElement input_penghasilanTotal;
+	public WebElement input_totalIncome;
 
 	@FindBy(xpath = "//input[@placeholder='Pengeluaran']")
-	public WebElement input_pengeluaran;
+	public WebElement input_expense;
 	
 	@FindBy(xpath = "//select[@id='waktu']")
-	public WebElement select_waktu;
+	public WebElement select_term;
 	
 	@FindBy(xpath = "//div[@id='hitung_harga_button']/button")
-	public WebElement button_hitung;
+	public WebElement button_calculate;
 	
 	@FindBy(xpath = "//div[@id='harga_hasil']/h3")
-	public WebElement div_hasilHarga;
+	public WebElement div_price;
 	
 	@FindBy(xpath = "//p[text()='Isi kurang dari nilai sebelumnya']")
 	public WebElement p_annotation;
